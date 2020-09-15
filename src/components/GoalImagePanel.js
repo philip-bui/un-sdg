9c7df49617d697ce0069fd4f9c0ebf13896df94c
Philip Bui <philip.bui.developer@gmail.com>
2020-09-16 00:00:00 +1000

    Added Goal Image Panel Component

---
+++ src/components/GoalImagePanel.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { View, TouchableOpacity, Image, StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  view: {
+    width: "50%",
+    height: 170,
+    padding: 8,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+});
+
+export default ({ source, style, onPress, ...props }) => (
+  <View style={StyleSheet.flatten([styles.view, style])} {...props}>
+    <TouchableOpacity
+      activeOpacity={0.6}
+      onPress={onPress}
+      disabled={onPress == null}
+    >
+      <Image style={styles.image} resizeMode="contain" source={source} />
+    </TouchableOpacity>
+  </View>
+);
