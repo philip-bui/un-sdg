d058afa0edb2590c9437da28122c575062386bf6
Philip Bui <philip.bui.developer@gmail.com>
2020-09-15 12:00:00 +1000

    Added ESLint config, removed Flow

---
+++ App.test.js
@@ -0,0 +1,11 @@
+import "react-native";
+import React from "react";
+import renderer from "react-test-renderer";
+import App from "./App";
+
+// Note: test renderer must be required after react-native.
+describe("App", () => {
+  it("renders correctly", () => {
+    expect(() => renderer.create(<App />)).not.toThrow();
+  });
+});
