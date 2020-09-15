600136d5a8f29c828b6261fb036de85b02cf1296
Philip Bui <philip.bui.developer@gmail.com>
2020-09-15 14:00:00 +1000

    Added react-navigation-tabs

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 import "react-native-gesture-handler";
 import React from "react";
-import { StatusBar } from "react-native";
+import { StatusBar, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+const Tab = createBottomTabNavigator();
 
 export default () => (
   <NavigationContainer>
     <StatusBar barStyle="dark-content" />
+    <Tab.Navigator initialRouteName="The Goals">
+      <Tab.Screen name="News" component={View} />
+      <Tab.Screen name="The Goals" component={View} />
+      <Tab.Screen name="Actions" component={View} />
+      <Tab.Screen name="Partners" component={View} />
+      <Tab.Screen name="Profile" component={View} />
+    </Tab.Navigator>
   </NavigationContainer>
 );
---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { View } from "react-native";
+
+export default class HomeScreen extends React.PureComponent {
+  render() {
+    return <View />;
+  }
+}
